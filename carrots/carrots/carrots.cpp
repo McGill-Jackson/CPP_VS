38376c646dd7b6ae0022bb879e736395024cbb24
史晨亮 <1823793113@qq.com>
2021-04-16 17:41:17 +0800

    1,1

--- carrots/carrots/carrots.cpp
+++ carrots/carrots/carrots.cpp
@@ -1,18 +1,18 @@
 //carrots.cpp -- food processing program
 //using and displays a variable
+//input and output
 #include <iostream>
 
 int main() {
 	using namespace std;
 	
 	int carrots;			//declare an integer variable
-
-	carrots = 25;			//assign a value to the variable
-	cout << "I have ";
-	cout << carrots;		//display a value of the variable
-	cout << " carrots";
-	cout << endl;
-	carrots = carrots - 1;	//modify the variable
-	cout << "Crunch,crunch.Now I have " << carrots << "carrots." << endl;
+	
+	cout << "How many carrots do you have?" << endl;
+	cin >> carrots; //C++ input
+	cout << "Here are two more.";
+	carrots = carrots + 2;
+	//the next line cancatenates output
+	cout << "Now you have " << carrots << "carrots." << endl;
 	return 0;
 }
