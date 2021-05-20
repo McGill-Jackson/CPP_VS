56fcd89b9b513d09d989bf41d35dd063326ceff5
史晨亮 <1823793113@qq.com>
2021-05-20 21:47:31 +0800

    1.0

--- RBTree/RBTree/test.cpp
+++ RBTree/RBTree/test.cpp
@@ -5,26 +5,59 @@ enum Color {
 	RED,
 	BLACK
 };
-template <class K,class V>
+template <class V>
 struct RBTreeNode {
-	RBTreeNode<K, V>* _left;	//left child
-	RBTreeNode<K, V>* _right;	//right child
-	RBTreeNode<K, V>* _parent;	//parent
-	pair<K, V> _kv;//key-value
+	RBTreeNode<V>* _left;	//left child
+	RBTreeNode<V>* _right;	//right child
+	RBTreeNode<V>* _parent;	//parent
+	V _val;//key-value
 	Color _color; //color
 
-	RBTreeNode(const pair<K,V>& kv = pair<K,V>())
-		:_kv(kv)
+	RBTreeNode(const V& val = V())
+		:_val(val)
 		,_color(RED)
 		,_left(nullptr)
 		,_right(nullptr)
 		,_parent(nullptr)
 	{}
 };
+template <class V>
+struct RBTreeIterator {
+	typedef RBTreeNode<V> Node;
+	typedef RBTreeIterator<V> Self;
+	typedef Node* pNode;
+	RBTreeIterator(pNode node)
+		:_node(node)
+	{}
+	pNode _node;
 
-template <class K,class V>
+	//解引用
+	V& operator*{
+		return _node->val;
+	}
+
+	Self operator--{
+		if (_node->_left) {
+			_node = _node->_left;
+			while (_node->_right) {
+				_node = _node->_right;
+			}
+		}
+		else {
+			pNode parent = _node->_parent;
+			while (parent->_left == _node) {
+				_node = parent;
+				parent = _node->parent;
+			}
+			if (_node->_left != parent)
+				_node = parent;
+		}
+	}
+
+};
+template <class K,class V,class KeyOfValue>
 class RBTree {
-	typedef RBTreeNode<K, V> Node;
+	typedef RBTreeNode<V> Node;
 	typedef Node* pNode;
 public:
 	RBTree()
@@ -34,9 +67,16 @@ public:
 		//leftmostand rightmost parts of the tree.
 		_header->_left = _header->_right = _header;
 	}
-	bool insert(const pair<K, V>& kv);
+	bool insert(const V& val);
 	void _validation() {
 		_inOrder(_header->_parent);
+		bool is = IsValidRBTree();
+		if (is == true) {
+			cout << "Meet" << endl;
+		}
+		else {
+			cout << "Not meet" << endl;
+		}
 	}
 private:
 	void _RotateL(pNode pParent);
@@ -44,17 +84,60 @@ private:
 	void swap(pNode& A, pNode& B);
 	void most();
 	void _inOrder(pNode parent);
+	bool IsValidRBTree();
+	bool _IsValidRBTree(pNode root, size_t k, const size_t blackCount);
+
 	pNode _header;
 };
-template <class K, class V>
-void RBTree<K, V>::_inOrder(pNode parent) {
+template <class K, class V, class KeyOfValue>
+bool RBTree<K,V,KeyOfValue>::_IsValidRBTree(pNode root, size_t k, const size_t blackCount) {
+	if (root == nullptr) {
+		if (k != blackCount) {
+			cout << "有路径上的黑色结点的个数不相同" << endl;
+			return false;
+		}
+		return true;
+	}
+	if (root->_color == BLACK)
+		++k;
+
+	pNode parent = root->_parent;
+	if (parent && parent->_color == RED && root->_color == RED) {
+		cout << "有红色结点相连" << endl;
+		return false;
+	}
+	return _IsValidRBTree(root->_left, k, blackCount) &&
+		_IsValidRBTree(root->_right, k, blackCount);
+}
+template <class K, class V, class KeyOfValue>
+bool RBTree<K, V, KeyOfValue>::IsValidRBTree() {
+	pNode root = _header->_parent;
+	if (root == nullptr)return true;
+
+	if (BLACK != root->_color) {
+		cout << "根节点不是黑色" << endl;
+		return false;
+	}
+
+	size_t blackCount = 0;
+	pNode cur = root;
+	while (cur) {
+		if (BLACK == cur->_color)
+			++blackCount;
+		cur = cur->_left;
+	}
+	size_t k = 0;
+	return _IsValidRBTree(root, k, blackCount);
+}
+template <class K, class V, class KeyOfValue>
+void RBTree<K, V, KeyOfValue>::_inOrder(pNode parent) {
 	if (parent == nullptr) return;
 	_inOrder(parent->_left);
-	cout << parent->_kv.first << " ";
+	cout << parent->_val.first << " ";
 	_inOrder(parent->_right);
 }
-template <class K, class V>
-void RBTree<K, V>::most() {
+template <class K, class V, class KeyOfValue>
+void RBTree<K, V, KeyOfValue>::most() {
 	pNode most_L = nullptr;
 	pNode most_R = nullptr;
 	pNode most = _header->_parent;
@@ -70,15 +153,15 @@ void RBTree<K, V>::most() {
 	}
 	_header->_right = most_R;
 }
-template <class K, class V>
-void RBTree<K, V>::swap(pNode& A, pNode& B) {
+template <class K, class V, class KeyOfValue>
+void RBTree<K, V, KeyOfValue>::swap(pNode& A, pNode& B) {
 	pNode N;
 	N = A;
 	A = B;
 	B = N;
 }
-template <class K,class V>
-void RBTree<K, V>::_RotateR(pNode pParent) {
+template <class K,class V, class KeyOfValue>
+void RBTree<K, V, KeyOfValue>::_RotateR(pNode pParent) {
 	pNode pSubL = pParent->_left;
 	pNode pSubLR = pSubL->_right;
 
@@ -101,8 +184,8 @@ void RBTree<K, V>::_RotateR(pNode pParent) {
 	}
 	pParent->_parent = pSubL;
 }
-template <class K,class V>
-void RBTree<K,V>::_RotateL(pNode pParent) {
+template <class K,class V, class KeyOfValue>
+void RBTree<K,V, KeyOfValue>::_RotateL(pNode pParent) {
 	pNode pSubR = pParent->_right;
 	pNode pSubRL = pSubR->_left;
 
@@ -124,12 +207,12 @@ void RBTree<K,V>::_RotateL(pNode pParent) {
 	}
 	pParent->_parent = pSubR;
 }
-template <class K, class V>
-bool RBTree<K,V>::insert(const pair<K, V>& kv) {
+template <class K, class V, class KeyOfValue>
+bool RBTree<K,V, KeyOfValue>::insert(const V& val) {
 	//insert
 	//1.When the tree is empty
 	if (_header->_parent == nullptr) {
-		pNode root = new Node(kv);
+		pNode root = new Node(val);
 		_header->_parent = root;
 		root->_parent = _header;
 		_header->_left = _header->_right = root;
@@ -139,14 +222,15 @@ bool RBTree<K,V>::insert(const pair<K, V>& kv) {
 	}
 	pNode parent = nullptr;
 	pNode cur = _header->_parent;
+	KeyOfValue kov;
 	while (cur) {
 		parent = cur;
-		if (kv.first == cur->_kv.first) return false;
-		else if (kv.first < cur->_kv.first) cur = cur->_left;
+		if (kov(val) == kov(cur->_val)) return false;
+		else if (kov(val) < kov(cur->_val)) cur = cur->_left;
 		else cur = cur->_right;
 	}
-	cur = new Node(kv);
-	if (parent->_kv.first > kv.first)
+	cur = new Node(val);
+	if (kov(parent->_val) > kov(val))
 		parent->_left = cur;
 	else
 		parent->_right = cur;
@@ -199,14 +283,60 @@ bool RBTree<K,V>::insert(const pair<K, V>& kv) {
 	_header->_parent->_color = BLACK;
 	_header->_parent->_parent = _header;
 }
-void test() {
-	RBTree<int, int >rbt;
-	int arr[9] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
-	for (int i = 0; i < 9; i++) {
-		rbt.insert(make_pair(arr[i], arr[i]));
+template <class K,class T>
+class Map {
+	struct MapKeyOfValue{
+		const K& operator()(const pair<K, T>& val) {
+			return val.first;
+		}
+	};
+public:
+	bool insert(const pair<K, T>& kv) {
+		return _rbt.insert(kv);
 	}
-	rbt._validation();
+	/*T& oprator[](const K& key) {
+		bool ret = _rbt.insert(make_pair(key, T()));
+	}*/
+private:
+	typedef RBTree<K, pair<K, T>, MapKeyOfValue> rbt;
+	rbt _rbt;
+};
+template <class K>
+class Set {
+	struct SetKeyOfValue {
+		const K& operator()(const K& val) {
+			return val;
+		}
+	};
+public:
+	bool insert(const K& val) {
+		return _rbt.insert(val);
+	}
+private:
+	typedef RBTree<K, K, SetKeyOfValue> rbt;
+	rbt _rbt;
+};
+void test() {
+	Map<int, int >m;
+	m.insert(make_pair(1, 1));
+	m.insert(make_pair(2, 1));
+	m.insert(make_pair(3, 1));
+	m.insert(make_pair(4, 1));
+
+	Set<int>s;
+	s.insert(1);
+	s.insert(2);
+	s.insert(3);
+
 }
+//void test() {
+//	RBTree<int, int >rbt;
+//	int arr[9] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+//	for (int i = 0; i < 9; i++) {
+//		rbt.insert(make_pair(arr[i], arr[i]));
+//	}
+//	rbt._validation();
+//}
 int main() {
 	test();
 	return 0;
